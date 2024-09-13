9e02654e71db6a5c428a9908850b0eaf55458cb1
Anders Sanderli <anders.sanderli@iths.se>
2024-09-13 13:37:39 +0200

    initial commit

---
+++ Random kod/kortlek.py
@@ -0,0 +1,22 @@
+import random
+
+def ny():
+    lek = []
+    for i in range(1,5):
+        for j in range(1,14):
+            lek.append((i,j))
+    random.shuffle(lek)
+    return lek
+
+def ge(lek):
+    if len(lek) > 0:
+        return lek.pop()
+    else:
+        return None
+färg = '\N{BLACK CLUB SUIT}\N{WHITE DIAMOND SUIT}'\
+       '\N{WHITE HEART SUIT}\N{BLACK SPADE SUIT}'
+valör = ('E','2','3','4','5','6','7','8','9','10','Kn','D','K')
+
+def visa(k, sist='\n'):
+    f,v=k
+    print(färg[f-1],valör[v-1], end=sist)
